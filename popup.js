b55e14fd5f8c72cca4c8bbe8886a62653f3f4c63
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-01-25 13:27:34 -0500

    Switch the sizing for elements on the graph to be dynamic

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
-import { drawLines } from "./scripts/shareButton.js";
+import { getClipboardImage } from "./scripts/shareButton.js";
 document.getElementById("shareButton").addEventListener("click", () => {
-    drawLines(1, 2);
+    // chrome.storage.local.get("rotTime");
+    // chrome.storage.local.get("productiveTime");
+    getClipboardImage(10.2, 2);
 });
--- scripts/shareButton.js
+++ scripts/shareButton.js
@@ -1,52 +1,80 @@
-export function drawLines(rotTime, productiveTime) {
-    let totalTime = rotTime + productiveTime;
-    let rotPrecent = rotTime / totalTime;
-    let productivePrecent = productiveTime / totalTime;
-
-    const width = 500;
-    const height = 500;
-
-    // Create a canvas
+export function createCanvas(width, height) {
     const canvas = document.createElement("canvas");
-    canvas.width = width; // Set desired width
-    canvas.height = height; // Set desired height
-    let ctx = canvas.getContext("2d");
+    canvas.width = width;
+    canvas.height = height;
+    return canvas;
+}
+
+export function generateGraph(canvas, rotTime, productiveTime) {
+    const totalTime = rotTime + productiveTime;
+    const rotPrecent = rotTime / totalTime;
+    const productivePrecent = productiveTime / totalTime;
+
+    const ctx = canvas.getContext("2d");
     
     // Draw the circle graph
-    const xPos = width / 2;
-    const yPos = height / 2;
+    const xPos = canvas.width / 2;
+    const yPos = canvas.height / 2;
     const rotAngle = Math.PI * 2 * rotPrecent;
     const productiveAngle = Math.PI * 2 * productivePrecent;
 
+    // Background
+    ctx.fillStyle = "rgb(36, 39, 58)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Title Text
+    ctx.font = "" + (width / 500) * 40 + "px Arial";
+    ctx.fillStyle = "rgb(202, 211, 245)";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+    ctx.fillText("Daily Rot Chart", width / 2, height / 10);
+
+    // Legend Symbols
+    ctx.font = "" + (width / 500) * 20 + "px Arial";
+    ctx.textAlign = "left";
+
+    const padding = canvas.width / 50;
+    const sqaureSize = canvas.width / 20;
+
+    ctx.fillStyle = "rgb(237, 135, 150)";
+    ctx.fillRect(padding, canvas.height - padding - sqaureSize, sqaureSize, sqaureSize);
+    ctx.fillText("Rot Time", padding * 2 + sqaureSize, canvas.height - padding - sqaureSize);
+
+    ctx.fillStyle = "rgb(166, 218, 149)";
+    ctx.fillRect(padding, canvas.height - sqaureSize * 2 - padding * 2, sqaureSize, sqaureSize);
+    ctx.fillText("Productive Time", padding * 2 + sqaureSize, canvas.height - padding - sqaureSize);
+
     // Stroke settings
-    ctx.lineWidth = 50;
-    ctx.shadowColor = "rgba(0, 0, 0, 0.1)"; 
+    ctx.lineWidth = (canvas.width / 10);
+    ctx.shadowColor = "rgba(0, 0, 0, 0.2)"; 
     ctx.shadowBlur = 10; 
     ctx.shadowOffsetX = 5; 
     ctx.shadowOffsetY = 5;
 
-    ctx.strokeStyle = "rgb(255,120,120)"
+    ctx.strokeStyle = "rgb(237, 135, 150)"
+
+    const radius = canvas.width / 5;
 
     ctx.beginPath();
-    ctx.arc(xPos, yPos, 100, 0, rotAngle);
+    ctx.arc(xPos, yPos, radius, 0, rotAngle);
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.strokeStyle = "rgb(154,208,112)"
-    ctx.arc(xPos, yPos, 100, rotAngle, rotAngle + productiveAngle);
+    ctx.strokeStyle = "rgb(166, 218, 149)"
+    ctx.arc(xPos, yPos, radius, rotAngle, rotAngle + productiveAngle);
     ctx.stroke();
+}
+export function getClipboardImage(rotTime, productiveTime) {
 
-    // Convert the canvas to a PNG file
-    canvas.toBlob((blob) => {
-      // Create a download link
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      let date = new Date();
-      a.href = url;
-      a.download = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "-rot-meter.png"; // File name
-      a.click();
-  
-      // Clean up
-      URL.revokeObjectURL(url);
-    }, "image/png");
+    const width = 500;
+    const height = 500;
+
+    const canvas = createCanvas(width, height);
+
+    generateGraph(canvas, rotTime, productiveTime);
+
+    canvas.toBlob(async (blob) => {
+        const chartImage= new ClipboardItem({"image/png": blob});
+        await navigator.clipboard.write([chartImage]);
+    });
 }
