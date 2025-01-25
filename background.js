e8ee89ecd556f075bbe5f0802d29baf4b4973825
efebolukbasi <bolukbasi.efe1@gmail.com>
2025-01-24 22:10:04 -0500

    Add base files for extensions

---
+++ background.js
@@ -0,0 +1,32 @@
+const productiveSites = ["docs.google.com", "stackoverflow.com", "khanacademy.org"];
+const unproductiveSites = ["facebook.com", "youtube.com","reddit.com"];
+
+// Function for checking currrent site productivity
+function checkSite(url){
+    const domain = new URL(url).hostname.replace("www.", "");
+    if (unproductiveSites.includes(domain)) {
+        console.log("Bad: This site might hurt your productivity.");
+        chrome.action.setBadgeText({ text: "Bad" });
+        chrome.action.setBadgeBackgroundColor({ color: "red" });
+    } else if (productiveSites.includes(domain)) {
+        console.log("Good: This site helps your productivity!");
+        chrome.action.setBadgeText({ text: "Good"});
+        chrome.action.setBadgeBackgroundColor({ color: "green" });
+    } else{
+        console.log("Neutral: Site not in productivity list.");
+        chrome.action.setBadgeText({ text: "" });
+    }
+}
+
+// Listening for tab activation
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+    const tab = await chrome.tabs.get(activeInfo.tabId);
+    if (tab.url) checkSite(tab.url);
+});
+
+// Listening for URL updates in active tab
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url && tab.active) {
+        checkSite(changeInfo.url);
+    }
+});
