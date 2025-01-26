4fbcc4e987cc523036a4b1fdee95d0b6d07d5121
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-01-26 02:32:22 -0500

    Add base of the API

--- server/app.py
+++ server/app.py
@@ -1,13 +1,56 @@
-from flask import Flask
-from endpoints import users
+from flask import Flask, jsonify, request, g
+from db import database
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(users.blueprint, url_prefix="/users")
     return app
 
 
+def get_db():
+    if not hasattr(g, 'db'):  # Check if db is already in the Flask global context
+        g.db = database.connect()
+    return g.db
+
+
+app = create_app()
+
+
+@app.route("/users", methods=["POST", "PUT", "GET"])
+def handle_users():
+    if request.method == "POST":
+        # ADD USER TO THE DATABASE
+        data = request.get_json()
+        username = data.get("username")
+
+        if username is None:
+            return jsonify({"Error": "Unauthorized"}), 401
+        database.add_user(get_db(), username)
+        return jsonify({"StatusCode": 200}), 200
+
+    elif request.method == "PUT":
+        data = request.get_json()
+        username = data.get("username")
+        rotTime = data.get("rotTime")
+
+        if username is None:
+            return jsonify({"Error": "Unauthorized"}), 401
+        if rotTime is None:
+            return jsonify({"Error": "Bad Request"}), 400
+
+        database.update_rot_time(get_db(), username, rotTime)
+        return jsonify({"StatusCode": 200}), 200
+
+    elif request.method == "GET":
+        return jsonify(database.get_top_rotters(get_db())), 200
+
+
+@app.teardown_appcontext
+def close_db(error):
+    db = getattr(g, '', None)
+    if db is not None:
+        db.close()
+
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
--- server/db/database.py
+++ server/db/database.py
@@ -2,47 +2,31 @@ import sqlite3
 
 
 def connect():
-    connection = sqlite3.connect("db/users.db")
-    cursor = connection.cursor()
-    return cursor
+    return sqlite3.connect("db/users.db")
 
 
-def setup_table(cursor):
-    cursor.execute(
-        "CREATE TABLE IF NOT EXIST 'users' (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL")
-    cursor.execute(
-        "CREATE TABLE IF NOT EXIST 'status' (id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, status TEXT NOT NULL), FOREIGN KEY (id) REFERENCES users(id);")
-    cursor.execute(
-        "CREATE TABLE IF NOT EXIST 'friends' (user_id INTEGER PRIMARY KEY, friend_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id), FOREIGN KEY (friend_id) REFERENCES users(id);")
+def setup_table(db):
+    with db:
+        db.cursor().execute(
+            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL, rotTime INTEGER);")
 
 
-def add_user(cursor, username):
-    cursor.execute("INSERT INTO users VALUES(?)", username)
+def add_user(db, username):
+    with db:
+        db.cursor().execute(
+            "INSERT INTO users (username, rotTime) VALUES(?, ?)", (username, 0))
 
 
-def add_status(cursor, user_id, username):
-    cursor.execute("INSERT INTO status VALUES(?, ?)", user_id, username, "")
+def update_rot_time(db, username, rotTime):
+    with db:
+        db.cursor().execute(
+            "UPDATE users SET rotTime = ? WHERE username = ?;", (rotTime, username))
 
 
-def add_friend(cursor, user_id, friend_name):
-    res = cursor.execute(
-        "SELECT id FROM users WHERE username = ?", friend_name)
-    if res is None:
-        print("add_friend: There was no friend")
-        return
-    friend_id = res.fetchone()
-    cursor.execute("INSERT INTO friends VALUES(?, ?)", user_id, friend_id)
-
-
-def update_status(cursor, user_id, isRot):
-    cursor.execute("UPDATE status SET status = ? WHERE id = ?",
-                   "Rotting" if isRot else "Productive", user_id)
-
-
-def get_friends_status(cursor, user_id):
-    res = cursor.execute(
-        "SELECT u.id, u.username FROM user u JOIN friends f ON u.id = f.friend_id WHERE f.user_id = ?", user_id)
-    if res is None:
-        print("add_friend: There was no friend")
-        return
-    print(res.fetchall())
+def get_top_rotters(db):
+    with db:
+        res = db.cursor().execute(
+            "SELECT * FROM users ORDER BY rotTime DESC LIMIT 10;")
+        if res is None:
+            return []
+        return res.fetchall()
