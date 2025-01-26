7420577c4cb5e7f29151d64ba5a2e09caedf8c2a
NerdBow <108768934+NerdBow@users.noreply.github.com>
2025-01-26 05:00:46 -0500

    Add leaderboard list with api refresh

--- extension/friends.js
+++ extension/friends.js
@@ -1,35 +1,44 @@
+import CONFIG from "./setting/config.js";
 // Sample friends list
-const friendsList = [
-    { user: "john_doe", status: "Rotting" },
-    { user: "jane_smith", status: "Productive" },
-    { user: "max_power", status: "Rotting" },
-    { user: "linda_bright", status: "Productive" },
-  ];
-  
-  // Reference to the friends list container
-  const friendsListContainer = document.createElement("div");
-  friendsListContainer.id = "friends-list-container";
-  
-  // Generate the list of friends dynamically
-  friendsList.forEach(friend => {
-    const friendElement = document.createElement("div");
-    friendElement.className = "friend-item";
-  
-    const nameElement = document.createElement("span");
-    nameElement.className = "friend-name";
-    nameElement.textContent = friend.user;
-  
-    const statusElement = document.createElement("span");
-    statusElement.className = "friend-status";
-    statusElement.textContent = ` (${friend.status})`;
-  
-    friendElement.appendChild(nameElement);
-    friendElement.appendChild(statusElement);
-    friendsListContainer.appendChild(friendElement);
-  });
-  
-  // Add the friends list to the page
-  document.getElementById("friends-list-page").appendChild(friendsListContainer);
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch("http://" + CONFIG.BACKEND_API + "/users", {method: "GET", headers: {"Content-Type": "application/json"}});
+
+    const data = await response.json();
+
+    // Reference to the friends list container
+    const friendsListContainer = document.createElement("div");
+    friendsListContainer.id = "friends-list-container";
+
+    const friendsList = data;
+    // Generate the list of friends dynamically
+    friendsList.forEach(friend => {
+      const friendElement = document.createElement("div");
+      friendElement.className = "friend-item";
+    
+      const nameElement = document.createElement("span");
+      nameElement.className = "friend-name";
+      nameElement.textContent = friend[1];
+    
+      const statusElement = document.createElement("span");
+      statusElement.className = "friend-status";
+      statusElement.textContent = ` (${friend[2]})`;
+    
+      friendElement.appendChild(nameElement);
+      friendElement.appendChild(statusElement);
+      friendsListContainer.appendChild(friendElement);
+    });
+    
+    // Add the friends list to the page
+    document.getElementById("friends-list-page").appendChild(friendsListContainer);
+  } catch (error) {
+    console.error("Error fetching friends list:", error);
+    return;
+  }
+});
+
   
   // Back button functionality
   document.getElementById("back-button").addEventListener("click", () => {
